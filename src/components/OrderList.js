1ae86f8111524435f1e8f40088be4246c69213a7
Muxac02 <mihail.pilipenko2002@mail.ru>
2025-03-19 00:55:46 +0300

    connecting telegram mini app

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -6,7 +6,7 @@ export default class OrderList extends Component {
     return (
       <div className='oder-list'>
         {this.props.data.map((el)=><OrderItem key={el.id} data={el} onItemAmountChange={this.props.onItemAmountChange}/>)}
-        {this.props.data.length>0&&<button className='order-submit-button' type='button' onClick={()=>console.log(this.props.data)}>Заказать</button>}
+        {this.props.data.length>0&&<button className='order-submit-button' type='button' onClick={()=>window.Telegram.WebApp.sendData(this.props.data)}>Заказать</button>}
       </div>
     )
   }
